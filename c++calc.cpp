08dcb11dff5c2e382e3c922038a27eb1db47c2e7
BaconBoi95 <baconboi95@icloud.com>
2020-03-12 15:17:18 -0400

    Update c++calc.cpp

--- c++calc.cpp
+++ c++calc.cpp
@@ -4,6 +4,7 @@ int choice;
 int long num1, num2, num3;
 int main()
 {
+#ifndef test
 cout << "WELCOME TO THE CALC\n";
 cout << "CHOOSE OPTION: \n";
 cout << "\n\n1.ADDITION";
@@ -12,7 +13,15 @@ cout << "\n3.MULTIPLICATION";
 cout << "\n4.DIVISON";
 cout << "\n\nCHOICE: ";
 cin >> choice;
-
+#endif
+#ifdef test
+int i;
+i=1;
+while (i!=4) {
+choice=i;
+num1=10;
+num2=4;
+#endif
 while(choice < 1 || choice > 4)
 {
 cout << "INVALID CHOICE\n";
@@ -26,10 +35,12 @@ switch(choice)
 {
 case 1:
 //Addition
+#ifndef test
 cout << "INPUT FIRST NUMBER: \n";
 cin >> num1;
 cout << "PLUS: \n";
 cin >> num2;
+#endif
 num3 = num1 + num2;
 a="+";
 //cout << "YOUR ANS IS: " << num3 << "\n";
@@ -37,10 +48,12 @@ break;
     
   case 2:
     //Subtraction
+#ifndef test
     cout << "INPUT FIRST NUMBER: \n";
 cin >> num1;
     cout << "MINUS: \n";
     cin >> num2;
+#endif
     num3 = num1 - num2;
 a="-";
 //    cout << "YOUR ANS IS: " << num3 << "\n";
@@ -48,10 +61,12 @@ a="-";
     
   case 3:
     //Multiplication
+#ifndef test
     cout << "INPUT FIRST NUMBER: \n";
       cin >> num1;
     cout << "TIMES: \n";
     cin >> num2;
+#endif
     num3 = num1 * num2;
 a="*";
 //    cout << "YOUR ANS IS: " << num3 << "\n";
@@ -59,14 +74,20 @@ a="*";
     
   case 4:
     //Divison
+#ifndef test
     cout << "INPUT FIRST NUMBER: \n";
     cin >> num1;
     cout << "DIVIDED BY: \n";
     cin >> num2;
+#endif
     num3 = num1 / num2;
 a="/";
 //    cout << "YOUR ANS IS: " << num3 << "\n";
     break;
 }
 cout << num1 << " " << a << " " << num2 << "=" << num3 << endl;
+#ifdef test
+i=i+1;
+}
+#endif
 }
